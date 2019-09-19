d48ab85f262cfde34ed28c5bfdb5898f941c5645
arx1987 <alexandrisaev1987@gmail.com>
2019-09-19 23:33:23 +0300

    последние изменения в "pik" на сегодня или может быть навсегда)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -508,7 +508,7 @@
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(224, 523);
+      this.ClientSize = new System.Drawing.Size(224, 428);
       this.Controls.Add(this.groupBox5);
       this.Controls.Add(this.groupBox4);
       this.Controls.Add(this.groupBox3);
