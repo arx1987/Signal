d221aabdd0049f0a94780ddcb99b56f8dd791b27
arx1987 <alexandrisaev1987@gmail.com>
2019-09-19 22:18:37 +0300

    первая фиксация

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SIgnal {
+  partial class Form1 {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.checkBox1 = new System.Windows.Forms.CheckBox();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(12, 29);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(64, 20);
+      this.textBox1.TabIndex = 0;
+      this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 10);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(140, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Укажите время в минутах";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 82);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "Старт";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.Button1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 56);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(0, 13);
+      this.label2.TabIndex = 3;
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+      // 
+      // checkBox1
+      // 
+      this.checkBox1.AutoSize = true;
+      this.checkBox1.Checked = true;
+      this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.checkBox1.Location = new System.Drawing.Point(82, 31);
+      this.checkBox1.Name = "checkBox1";
+      this.checkBox1.Size = new System.Drawing.Size(85, 17);
+      this.checkBox1.TabIndex = 4;
+      this.checkBox1.Text = "зациклить?";
+      this.checkBox1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+      this.checkBox1.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(200, 117);
+      this.Controls.Add(this.checkBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Timer timer1;
+    private System.Windows.Forms.CheckBox checkBox1;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIgnal {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ФИЧИ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      //-ДОПИСАТЬ ПОЛЕ С ЛЮБЫМ ТЕКСТОМ, КОТОРЫЙ БУДЕТ ВЫСВЕЧИВАТЬСЯ КОГДА ВРЕМЯ ИСТЕЧЕТ
+      //-как-то влепить туда английский
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      AutoCompleteStringCollection source = new AutoCompleteStringCollection()//добавляем функцию автокомплита для поля textBox1
+        {
+            "5",
+            "10",
+            "15",
+            "30",
+            "60"
+        };
+      textBox1.AutoCompleteCustomSource = source;
+      textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+      textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+    }
+    int timeLeft; //переменная, хранит значнеие оставшегося до сигнала времени
+    int buttonState = 0; /*переменная, которая содержит состояние кнопки. Если кнопка нажата впервые, то состояние 1, 
+      если кнопка нажата 2-ой раз до сигнала, то состояние 2(и счетчик временно останавливается), если кнопка нажата 
+      3-ий раз до сигнала, то состояние 3 и счетчик продолжает работать*/
+    bool textBox1Changed = false; //Устанавливаем тру, если значение поля textBox1 было изменено не в первый раз
+    int textBox1Changed1 = 0; //переменная, которая отслеживает количество изменений поля textBox1 
+
+    private void Button1_Click(object sender, EventArgs e) {
+      /*при нажатии на кнопку, берем значение из textBox1, проверяем, если оно не может быть преобразовано в целое число, очищаем textBox1, ставим в него курсор
+       и пишем сообщение в label1 - красным шрифтом, что нужно ввести число. Если в поле введено целое число, то стартуем таймер,
+       по окончании которого, должен быть подан звуковой сигнал*/
+      someChecks();
+      /*в будущем будет добавлена фича с английскими словами и может быть что-то еще*/
+    }
+
+    internal void someChecks() {
+      int res;//переменная, которая хранит значение введенное в поле textBox1 - если поле целочисленное в минутах
+      bool isInt = Int32.TryParse(textBox1.Text, out res);
+      if (isInt) {
+        label1.ForeColor = Color.Black;
+        label1.Text = "Укажите время в минутах";
+        if (button1.Text == "Старт") {
+          buttonState = 0;
+        }
+        buttonState++;
+        if (buttonState == 1) {
+          button1.Text = "Пауза";
+          timeLeft = res * 60;
+          timer1.Start();
+        }
+        else if ((buttonState > 1) && (buttonState % 2 == 0)) {
+          button1.Text = "Пуск";
+          timer1.Stop();
+        }
+        else if (buttonState > 1 && buttonState % 2 != 0) {
+          button1.Text = "Пауза";
+          timer1.Start();
+        }
+      }
+      else {
+        label1.ForeColor = Color.Red;
+        label1.Text = "Ниже введите целое число!";
+        textBox1.Text = "";
+        textBox1.Focus();
+      }
+    }
+
+    private void Timer1_Tick(object sender, EventArgs e) {
+      if(timeLeft > 0) {
+        //Display the new time left by updating the Time Left label.
+        timeLeft = timeLeft - 1;
+        int hoursLeft = timeLeft / 3600;
+        int minutesLeft = (timeLeft - hoursLeft * 3600) / 60;
+        int secondsLeft = timeLeft - minutesLeft * 60 - hoursLeft * 3600;
+        label2.Text = "Осталось " + hoursLeft + " ч " + minutesLeft + " м " + secondsLeft + " с";
+      } else {
+        //if the user ran out of time, stop the timer, play signal, clear textBox1, focus it, 
+        //and change button's text to Start.
+        Console.Beep(5000, 1000);//звуковой сигнал
+        timer1.Stop();
+        button1.Text = "Старт";
+        if (!checkBox1.Checked) {
+          label2.Text = "";
+        } else {          
+          someChecks();
+        }
+      }
+    }
+
+    private void TextBox1_TextChanged(object sender, EventArgs e) {
+      if (buttonState > 0) {//Если кнопка button1 была нажата хотя бы 1 раз и были внесены изменения в текстовое поле
+        textBox1Changed1++;
+        if (textBox1Changed1 > 0) {
+          textBox1Changed = true;
+        }
+        button1.Text = "Старт";
+      }
+    }
+  }
+}
